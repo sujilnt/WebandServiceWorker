3e93e85753ef78db3ab64008e95909e049020a25
sujilnt <sujilnt@gmail.com>
2019-07-21 03:34:15 +0100

    added service and written code to update

--- web/js/blog.js
+++ web/js/blog.js
@@ -4,10 +4,12 @@
 	var offlineIcon;
 	var isonline=("onLine" in navigator) ? navigator.onLine: true;
 	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
-
+	var usingSW = ("serviceWorker" in navigator);
+	var swRegistration;
+    var svcworker;
 	document.addEventListener("DOMContentLoaded",ready,false);
 
-
+    initServerWorker().catch(console.error);
 	// **********************************
 
 	function ready() {
@@ -18,11 +20,45 @@
 		window.addEventListener("online",function online() {
 			offlineIcon.classList.add("hidden");
 			isonline=true;
+			sendStatusUpdate();
 		});
 		window.addEventListener("offline",function offline() {
 			offlineIcon.classList.remove("hidden");
 			isonline=false;
+			sendStatusUpdate();
 		});
 	}
-
+	async function initServerWorker() {
+		swRegistration = await navigator.serviceWorker.register("/sw.js",{
+			updateViaCache: "none"
+		});
+		svcworker = swRegistration.installing || swRegistration.waiting || swRegistration.active;
+		navigator.serviceWorker.addEventListener("controllerchange", function onControllerChange(){
+			svcworker = navigator.serviceWorker.controller;
+			sendStatusUpdate(svcworker);
+		});
+		navigator.serviceWorker.addEventListener("message",onSWMessage);
+		
+	}
+	async function onSWMessage(){
+		var {data} =evt;
+		console.log("dataaaaaaaa",data);
+		if(data.requestStatusUpdate){
+		
+			console.log(`received status update request from service worker`);
+			sendStatusUpdate(evt.ports && evt.ports[0]);
+		}
+	}
+	function sendStatusUpdate(target){
+		sendSWMessage({ statusUpdate: {isonline,isLoggedIn} },target)
+	}
+	async function sendSWMessage(msg,target) {
+		if(target){
+			target.postMessage(msg);
+		}else if(svcworker){
+			svcworker.postMessage(msg)
+		} else{
+			navigator.serviceWorker.controller.postMessage(msg);
+		}
+	}
 })();
--- web/js/sw.js
+++ web/js/sw.js
@@ -1,3 +1,55 @@
 "use strict";
 
 // TODO
+const version =2;
+
+var  isOnline = true;
+var isLoggedIn= false;
+
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+self.addEventListener("message",onMessage);
+
+
+main().catch(console.error);
+
+//************************************************
+async function sendMessage(msg){
+	var allClients = await Clients.matchAll({
+		includeUncontrolled: true
+	});
+	return Promise.all(
+		allClients.map(function clientMsg(){
+			var chan = new MessageChannel();
+			chan.port1.onmessage = onMessage();
+			return client.postMessage(msg,[chan.port2]);
+			
+		})
+	)
+}
+function onMessage({data}){
+	if(data.statusUpdate){
+		({isOnline,isLoggedIn}= data.statusUpdate);
+		console.log(`service worker ${version} status update isOnline ${isOnline} and is ${isLoggedIn}`)
+	}
+}
+async function main(){
+	await sendMessage({
+		requestStatusUpdate: true
+	});
+	console.log(`server Worker (${version}) is starting `);
+}
+async function onInstall(evt){
+	console.log(`server Worker (${version}) is installed `);
+	self.skipWaiting();
+}
+
+function onActivate(evt){
+	evt.waitUntil(handleActivation());
+}
+async function handleActivation(){
+	await clients.claim();
+	console.log(`server Worker (${version}) is activated `);
+}
+
+
